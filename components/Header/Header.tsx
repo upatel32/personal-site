22f49d471f29cec0143ebc5533f681b58cd41b62
Urmit Patel <up112@protonmail.com>
2021-01-21 23:22:02 -0500

    adds basic info

---
+++ components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "next/head";
+
+const Header = () => (
+  <Head>
+    <title>UP</title>
+    <link rel="icon" href="/favicon.ico" />
+    <header>Header</header>
+  </Head>
+);
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,46 @@
-import Head from "next/head";
-import Footer from "../components/Footer";
+import Footer from "../components/Footer/Footer";
+import { createClient } from "urql";
+import Header from "../components/Header/Header";
+import { profileEnd } from "console";
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>UP</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home = ({ profile }) => {
+  console.log(profile);
+  return (
+    <div>
+      <Header />
+      <pre>{JSON.stringify(profile, null, 2)}</pre>
+      <Footer />
+    </div>
+  );
+};
 
-    <Footer />
-  </div>
-);
+const UserQuery = `
+query {
+  user(login: "upatel32") {
+    id
+    websiteUrl
+    bio
+    company
+    email
+  }
+}
+`;
+
+export async function getStaticProps() {
+  const client = createClient({
+    url: "https://api.github.com/graphql",
+    fetchOptions: {
+      headers: { authorization: `Bearer ${process.env.GITHUB_PAC}` },
+    },
+  });
+  const profile = await client.query(UserQuery).toPromise();
+
+  console.log(profile);
+  return {
+    props: {
+      profile: profile.data,
+    },
+  };
+}
 
 export default Home;
